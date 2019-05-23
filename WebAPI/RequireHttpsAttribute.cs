5531d16822357c771298a865f5b6b99c945882e4
Jeffrey Hermosa (ECICS <jvh@ecics.com.sg>
2019-05-23 12:01:34 +0800

    Add project files.

---
+++ WebAPI/RequireHttpsAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebAPI
+{
+    public class RequireHttpsAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+
+            }
+            base.OnAuthorization(actionContext);
+        }
+    }
+}
